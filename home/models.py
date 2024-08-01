0128728d4ddb0e74164d2d8f03fc47a37d4aeaf2
NoNathan17 <nathanong11217@gmail.com>
2024-07-31 18:37:21 -0700

    removed extra home file

--- home/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Player(models.Model):
-    name = models.CharField(max_length=100)
-    team = models.CharField(max_length=50)
-    position = models.CharField(max_length=20)
-    

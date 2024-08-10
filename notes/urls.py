d0795ea34ed60ddd95d5d68a43f44c72eb5d670b
NoNathan17 <nathanong11217@gmail.com>
2024-08-09 18:55:23 -0700

    better style for notes

--- notes/urls.py
+++ notes/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('notes/', views.notes_view, name='notes'),
+    path('notes/<int:pk>', views.notes_detail, name='notes_detail')
+
 ]
--- notes/views.py
+++ notes/views.py
@@ -6,3 +6,7 @@ from .models import Note
 def notes_view(request):
     notes = Note.objects.all()
     return render(request, 'notes.html', {'notes': notes})
+
+def notes_detail(request, pk):
+    note = Note.objects.get(pk=pk)
+    return render(request, 'notes_detail.html', {'note': note})

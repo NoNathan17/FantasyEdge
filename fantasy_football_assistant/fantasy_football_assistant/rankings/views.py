9607a08d6cd8244faa8a10bfb9b0002b473b1311
NoNathan17 <nathanong1217@gmail.com>
2024-10-02 09:21:43 -0700

    removed unnecessary files

--- fantasy_football_assistant/fantasy_football_assistant/rankings/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-import requests
-
-# Create your views here.
-def fetch_data(request):
-    url = 'https://api.fantasynerds.com/v1/nfl/tiers?apikey=TEST&format='
-    response = requests.get(url)
-    players = response.json()
--- fantasy_football_assistant/rankings/management/commands/tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-
-            players.append(player_data)

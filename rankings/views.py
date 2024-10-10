efad1b15b5b151eeb2870baaa16c63c09c78264b
NoNathan17 <nathanong1217@gmail.com>
2024-10-09 20:26:48 -0700

    Pagination for rankings

--- rankings/views.py
+++ rankings/views.py
@@ -1,33 +1,63 @@
 from django.shortcuts import render, HttpResponse
 from django.template import loader
+from django.core.paginator import Paginator
 from .models import Player, Quarterback, RunningBack, WideReciever, TightEnd, Kicker, Defense
 
 # Create your views here.
 def rankings_view(request): 
     players = Player.objects.all()
-    return render(request, 'overall_rankings.html', {'players': players})
+    paginator = Paginator(players, 100)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number) 
+    return render(request, 'overall_rankings.html', {'page_obj': page_obj})
 
 def quarterback_view(request):
     quarterbacks = Quarterback.objects.all()
-    return render(request, 'quarterback_rankings.html', {'quarterbacks': quarterbacks})
+    paginator = Paginator(quarterbacks, 100)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number) 
+    return render(request, 'quarterback_rankings.html', {'page_obj': page_obj})
 
 def runningback_view(request):
     runningbacks = RunningBack.objects.all()
-    return render(request, 'runningback_rankings.html', {'runningbacks': runningbacks})
+    paginator = Paginator(runningbacks, 100)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number) 
+    
+    return render(request, 'runningback_rankings.html', {'page_obj': page_obj})
 
 def widereciever_view(request):
     widerecievers = WideReciever.objects.all()
-    return render(request, 'widereceiver_rankings.html', {'widerecievers': widerecievers})
+    paginator = Paginator(widerecievers, 100)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number) 
+    return render(request, 'widereceiver_rankings.html', {'page_obj': page_obj})
 
 def tightend_view(request):
     tightends = TightEnd.objects.all()
-    return render(request, 'tightend_rankings.html', {'tightends': tightends})
+    paginator = Paginator(tightends, 100)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number) 
+    return render(request, 'tightend_rankings.html', {'page_obj': page_obj})
 
 def kicker_view(request):
     kickers = Kicker.objects.all()
-    return render(request, 'kicker_rankings.html', {'kickers': kickers})
+    paginator = Paginator(kickers, 100)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number) 
+    return render(request, 'kicker_rankings.html', {'page_obj': page_obj})
 
 def defense_view(request):
     defenses = Defense.objects.all()
-    return render(request, 'defense_rankings.html', {'defenses': defenses})
+    paginator = Paginator(defenses, 100)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number) 
+    return render(request, 'defense_rankings.html', {'page_obj': page_obj})
 

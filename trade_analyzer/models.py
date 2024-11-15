5cbc1400469e9dc56ddb3fb99e3a2fa5283745a1
NoNathan17 <nathanong1217@gmail.com>
2024-11-15 01:28:38 -0800

    Created model for player in trade analyzer app

--- trade_analyzer/models.py
+++ trade_analyzer/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Player(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    team = models.CharField(max_length=50)
+    position = models.CharField(max_length=20)
+    info = models.URLField(max_length=200, default='N/A')
+    rating = models.DecimalField(max_digits=4, decimal_places=1, default=0.0)

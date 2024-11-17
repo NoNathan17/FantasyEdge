a00fd2e481cbbe594819366ef0ea401b2bbc78e5
NoNathan17 <nathanong1217@gmail.com>
2024-11-16 23:10:02 -0800

    Fixed basic functionalityb and styles for trade analyzer

--- trade_analyzer/urls.py
+++ trade_analyzer/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('trade_analyzer/', views.trade_analyzer_view, name='trade_analyzer'),
+    path('compare/', views.compare_trade, name='compare_trade'),
 ]
--- trade_analyzer/views.py
+++ trade_analyzer/views.py
@@ -6,7 +6,7 @@ from trade_analyzer.models import Player
 def trade_analyzer_view(request):
     return render(request, 'trade_analyzer.html')
 
-def compare_players(player_1, player_2):
+def compare_players(player_1: Player, player_2: Player) -> str:
     if player_1.rating > player_2.rating:
         return f"{player_1.name} wins the trade"
     elif player_1.rating < player_2.rating:
@@ -16,24 +16,15 @@ def compare_players(player_1, player_2):
     
 def compare_trade(request):
     if request.method == "POST":
-        # Get selected player IDs from the form
-        player_1_id = request.POST.get('player_1')
-        player_2_id = request.POST.get('player_2')
+        player_1_name = request.POST.get('player_1')
+        player_2_name = request.POST.get('player_2')
 
-        # Retrieve the players from the database
-        try:
-            player_1 = Player.objects.get(id=player_1_id)
-            player_2 = Player.objects.get(id=player_2_id)
+        player_1 = Player.objects.get(name=player_1_name)
+        player_2 = Player.objects.get(name=player_2_name)
 
-            # Call the compare_players function
-            result = compare_players(player_1, player_2)
+        result = compare_players(player_1, player_2)
 
-        except Player.DoesNotExist:
-            result = "One or both players do not exist."
-
-        # Return the result along with players list to the template
         return render(request, 'trade_analyzer.html', {'players': Player.objects.all(), 'result': result})
 
     else:
         return render(request, 'trade_analyzer.html', {'players': Player.objects.all()})
-    

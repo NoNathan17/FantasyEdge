60d535471aa14c90ae7f4550c080bbb4155c6f96
NoNathan17 <nathanong1217@gmail.com>
2024-12-22 19:52:26 -0800

    auth for notes feature

--- notes/views.py
+++ notes/views.py
@@ -1,34 +1,43 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Note
 from .forms import NoteForm
 
 # Create your views here.
 
+@login_required
 def notes_view(request):
-    notes = Note.objects.all()
+    notes = Note.objects.filter(user=request.user)
     return render(request, 'notes.html', {'notes': notes})
 
+@login_required
 def note_detail(request, pk):
-    note = Note.objects.get(pk=pk)
+    note = Note.objects.get(pk=pk, user=request.user)
     return render(request, 'note_detail.html', {'note': note})
 
+@login_required
 def note_create(request):
     if request.method == 'POST':
         form = NoteForm(request.POST)
-        form.save()
+        if form.is_valid():
+            note = form.save(commit=False)
+            note.user = request.user
+            form.save()
         return redirect('notes')
     else:
         form = NoteForm()
     return render(request, 'note_form.html', {'form': form})
 
+@login_required
 def note_delete(request, pk):
-    note = Note.objects.get(pk=pk)
+    note = Note.objects.get(pk=pk, user=request.user)
     if request.method == 'POST':
         note.delete()
         return redirect('notes')
 
+@login_required
 def note_edit(request, pk):
-    note = Note.objects.get(pk=pk)
+    note = Note.objects.get(pk=pk, user=request.user)
 
     if request.method == "POST":
         form = NoteForm(request.POST, instance=note)

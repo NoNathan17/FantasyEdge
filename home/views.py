55ed078e993561ad1cba95e5ed0fc3f77e4bef08
NoNathan17 <nathanong1217@gmail.com>
2024-12-22 20:06:30 -0800

    coming soon page

--- home/views.py
+++ home/views.py
@@ -5,3 +5,6 @@ from news.models import News
 def home_view(request):
     news_items = News.objects.all()
     return render(request, 'home.html', {'news_items': news_items})
+
+def coming_soon_view(request):
+    return render(request, 'coming_soon.html')

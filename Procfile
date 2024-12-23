e0739a8731605ad3a10b02a2c6d7e0a6ca83397c
NoNathan17 <nathanong1217@gmail.com>
2024-12-22 21:06:45 -0800

    style changes and preparing to deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn fantasy_football_assistant.wsgi
--- fantasy_football_assistant/settings.py
+++ fantasy_football_assistant/settings.py
@@ -128,6 +128,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'

3ba0b5f4c77e02b62f300cf825bdce92ba407571
NoNathan17 <nathanong1217@gmail.com>
2024-12-22 21:26:25 -0800

    deployment

--- fantasy_football_assistant/settings.py
+++ fantasy_football_assistant/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -51,7 +50,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleWare',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -78,7 +76,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'fantasy_football_assistant.wsgi.application'
+WSGI_APPLICATION = 'fantasy_football_assistant.wsgi.app'
 
 
 # Database
